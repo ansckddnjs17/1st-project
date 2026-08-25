12130037d54b9988dcb8830fd09ccbf656d5c4ec
grammm <gram.mermar501@gmail.com>
2026-08-25 12:28:38 +0900

    customer

--- back/src/main/java/org/back/back/domain/customer/entity/Customer.java
+++ back/src/main/java/org/back/back/domain/customer/entity/Customer.java
@@ -1,4 +1,37 @@
 package org.back.back.domain.customer.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.back.back.domain.order.entity.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String postcode;
+
+    public Customer(String email, String address, String postcode) {
+        this.email = email;
+        this.address = address;
+        this.postcode = postcode;
+    }
+
+    @OneToMany(mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
 }
